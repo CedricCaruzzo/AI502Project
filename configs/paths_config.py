eee66ade536ec64244dabb54f95461ba93308392
juchul.shin <skyjedi21@gmail.com>
2023-12-13 13:18:18 +0000

    first upload

---
+++ configs/paths_config.py
@@ -0,0 +1,12 @@
+dataset_paths = {
+    'celeba_test': 'pretrained_models/celeb',
+    'ffhq': 'pretrained_models/ffhq/ffhq256',
+}
+
+model_paths = {
+    'pretrained_psp_encoder': 'pretrained_models/psp_ffhq_encode.pt',
+    'ir_se50': 'pretrained_models/model_ir_se50.pth',
+    'stylegan_ffhq': 'pretrained_models/stylegan2-ffhq-config-f.pt',
+    'shape_predictor': 'pretrained_models/shape_predictor_68_face_landmarks.dat',
+    'age_predictor': 'pretrained_models/dex_age_classifier.pth'
+}
